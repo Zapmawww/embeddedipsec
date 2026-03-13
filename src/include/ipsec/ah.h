7b01256c652d8007ac4c1ac99c6f77f517db751b
Zapmawww <1626426326@qq.com>
2026-03-13 16:36:17 +0800

    Append Zapmawww copyright to modified files

--- src/include/ipsec/ah.h
+++ src/include/ipsec/ah.h
@@ -1,6 +1,7 @@
 /*
  * embedded IPsec
  * Copyright (c) 2003 Niklaus Schild and Christian Scheurer, HTI Biel/Bienne
+ * Copyright (c) 2026 Zapmawww
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
